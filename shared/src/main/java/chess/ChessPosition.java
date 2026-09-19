230a2662638b292afbaa6ad3ed3add1da9e2e348
Rahul Chaurasiya <rc08042005@gmail.com>
2026-09-18 23:48:31 -0600

    IMplemented chessposition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,4 +1,6 @@
 package chess;
+import java.util.Objects;
+
 
 /**
  * Represents a single square position on a chess board
@@ -7,8 +9,12 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private final int row;
+    private final int col;
 
     public ChessPosition(int row, int col) {
+    this.row =row;
+    this.col=col;
     }
 
     /**
@@ -16,7 +22,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +30,25 @@ public class ChessPosition {
      * 1 codes for the left column
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+       return col;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof ChessPosition)) {
+            return false;
+        }
+
+        ChessPosition other = (ChessPosition) obj;
+
+        return row == other.row && col == other.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 }
