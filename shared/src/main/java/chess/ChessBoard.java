d3eec33cef32998851c131a186e5910cc5270492
Rahul Chaurasiya <rc08042005@gmail.com>
2026-09-20 23:11:07 -0600

    ChessBoard initial finish

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,6 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private final ChessPiece[][] squares= new ChessPiece[8][8];
 
     public ChessBoard() {
         
@@ -19,7 +20,8 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+
+        squares[position.getRow()-1][position.getColumn()-1]=piece;
     }
 
     /**
@@ -30,7 +32,8 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+
+        return squares[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
@@ -38,6 +41,39 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+// clear pieces
+        for(int row=0;row<8;row++){
+            for(int col=0;col<8;col++){
+                squares[row][col]=null;
+      ;      }
+        }
+        // pawn placement
+        for (int col=1; col<=8;col++){
+            addPiece(new ChessPosition(2,col),
+            new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN));
+
+            addPiece(new ChessPosition(7,col),
+                    new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+        }
+        //placing the other piece in their starting position.
+        ChessPiece.PieceType[] backRow={
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK
+        };
+        for (int col=1; col<=8;col++){
+            ChessPiece.PieceType type =backRow[col-1];
+
+            addPiece(new ChessPosition(1,col),
+                    new ChessPiece((ChessGame.TeamColor.WHITE, type));
+            addPiece(new ChessPosition(8,col),
+                    new ChessPiece(ChessGame.TeamColor.BLACK, type));
+            );
+        }
     }
 }
