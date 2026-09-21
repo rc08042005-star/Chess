ec602087873eb73051d6199b9def25859612db94
Rahul Chaurasiya <rc08042005@gmail.com>
2026-09-20 19:39:15 -0600

    Implemented CHessmove

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,4 +1,5 @@
 package chess;
+import java.util.Objects;
 
 /**
  * Represents moving a chess piece on a chessboard
@@ -7,23 +8,31 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
+    private final ChessPosition startPosition;
+    private final ChessPosition endPosition;
+    private final ChessPiece.PieceType promotionPiece;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition= startPosition;
+        this.endPosition=endPosition;
+        this.promotionPiece=promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+
+       return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+
+        return endPosition;
     }
 
     /**
@@ -33,6 +42,21 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+
+        return promotionPiece;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (this==obj){
+            return true;
+        }
+        if (!(obj instanceof ChessMove other)){
+            return false;
+        }
+        return Objects.equals(startPosition, other.startPosition) && Objects.equals(endPosition, other.endPosition) && promotionPiece == other.promotionPiece;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(startPosition,endPosition,promotionPiece);
     }
 }
