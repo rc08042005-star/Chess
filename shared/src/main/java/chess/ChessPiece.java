84575c370d6d675e6d614b4cee4a01fa82cbc293
Rahul Chaurasiya <rc08042005@gmail.com>
2026-09-21 13:08:04 -0600

    Implemented ChessBoard and Chesspiece basics and all test in chessboard runs passed

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,8 +10,13 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,15 +35,16 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
+
 
     /**
      * Calculates all the positions a chess piece can move to
@@ -46,6 +53,24 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+    @Override
+    public boolean equals(Object obj){
+        if(this==obj){
+            return true;
+        }
+        if (!(obj instanceof ChessPiece other)){
+            return false;
+        }
+        return pieceColor==other.pieceColor && type==other.type;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(pieceColor, type);
+    }
+
+
+
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
     }
